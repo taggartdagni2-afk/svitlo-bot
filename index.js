933863bad16985df5990cbe448c12e9cbc53aa13
taggartdagni2-afk <taggartdagni2@gmail.com>
2026-02-06 19:22:37 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = process.env.BOT_TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "Бот працює ⚡");
+});
