788cd8d76ad46e68aa440025bd1f60aa9a73dcdd
taggartdagni2-afk <taggartdagni2@gmail.com>
2026-02-06 19:25:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-const TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require("node-telegram-bot-api");
+const { getDeviceStatus } = require("./ewelink");
 
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, "Бот працює ⚡");
+  bot.sendMessage(msg.chat.id, "Бот моніторингу світла запущено ⚡");
+});
+
+bot.onText(/\/status/, async (msg) => {
+  try {
+    const isOn = await getDeviceStatus();
+
+    if (isOn) {
+      bot.sendMessage(msg.chat.id, "⚡ Світло є!");
+    } else {
+      bot.sendMessage(msg.chat.id, "❌ Світла немає!");
+    }
+  } catch (error) {
+    bot.sendMessage(msg.chat.id, "Помилка перевірки світла 😢");
+    console.error(error);
+  }
 });
